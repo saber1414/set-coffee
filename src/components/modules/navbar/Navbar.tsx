bc6cec5b08d2d9dea28d86d91811b8dd6d5df788
Saber___Dev <saber.esmaili1414@gmail.com>
2025-08-16 10:01:59 +0330

    Add a new feature navbar

---
+++ src/components/modules/navbar/Navbar.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import Link from "next/link";
+import { IoIosArrowDown } from "react-icons/io";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsSuitHeart } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <main>
+          <div>
+            <Link href="/">
+              <img src="/images/logo.png" alt="Logo" />
+            </Link>
+          </div>
+          <ul className={styles.links}>
+            <li>
+              <Link href="/">صفحه اصلی</Link>
+            </li>
+            <li>
+              <Link href="/store">فروشگاه</Link>
+            </li>
+            <li>
+              <Link href="/blog">وبلاگ</Link>
+            </li>
+            <li>
+              <Link href="/about-me">درباره ما</Link>
+            </li>
+            <li>
+              <Link href="/contact-us">تماس با ما</Link>
+            </li>
+            <li>
+              <Link href="/roles">قوانین</Link>
+            </li>
+            {/* <li>
+              <Link href="/login-register">ورود / ثبت نام</Link>
+            </li> */}
+            <div className={styles.dropdown}>
+              <Link href="/p-user">
+                {" "}
+                <IoIosArrowDown className={styles.dropdown_icons} />
+                حساب کاربری
+              </Link>
+              <div className={styles.dropdown_content}>
+                <Link href="/p-user/orders">سفارشات</Link>
+                <Link href="/p-user/tickets">تیکت های پشتیبانی</Link>
+                <Link href="/p-user/comments">دیدگاه ها</Link>
+                <Link href="/p-user/favorites">علاقه مندی ها</Link>
+                <Link href="/p-user/account-details">جزئیات حساب</Link>
+              </div>
+            </div>
+          </ul>
+          <div className={styles.navbar_icons}>
+            <Link href="/cart">
+              <FiShoppingCart />
+              <span>0</span>
+            </Link>
+            <Link href="/favorites">
+              <BsSuitHeart />
+              <span>0</span>
+            </Link>
+          </div>
+        </main>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
